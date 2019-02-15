3b2db3b0c5ea1d5b543904ca872d19857448b613
PatrickSachs <kontakt@patrick-sachs.de>
2019-02-15 19:17:28 +0100

    Rebuilt current UI in Vue

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,75 @@
 //import SlowTreeGame from './Game';
 //import DemoCommand from './commands/DemoCommand';
 import Vue from 'vue'
-import Test from "./components/Test.vue";
+import App from "./components/App.vue";
 
-const gameDiv = document.getElementById('game')! as HTMLDivElement;
-if (!gameDiv) {
-    throw new Error('No #game div found!');
-}
-
-new Vue({
-    render: h => h(Test)
-  }).$mount(gameDiv)
+window.vue = new Vue({
+	render: h => h(App)
+}).$mount("#app")
 
 //window.game = new SlowTreeGame(gameDiv);
 
-var layout   = document.getElementById('layout'),
-    menu     = document.getElementById('menu'),
-    menuLink = document.getElementById('menuLink'),
-    content  = document.getElementById('game');
+const layout = document.getElementById('layout'),
+	menu = document.getElementById('menu'),
+	menuLink = document.getElementById('menuLink'),
+	content = document.getElementById('game');
 
 function toggleClass(element: HTMLElement | null, className: string) {
-    if (element === null) {
-        throw new Error("Variable element can not be empty!");
-    }
-    else {
-       var classes = element.className.split(/\s+/) || null,
-         length = classes.length,
-         i = 0;
+	if (element === null) {
+		throw new Error("Variable element can not be empty!");
+	}
+	else {
+		var classes = element.className.split(/\s+/) || null,
+			length = classes.length,
+			i = 0;
 
-    }
+	}
 
-    for(; i < length; i++) {
-        if (classes[i] === className) {
-        classes.splice(i, 1);
-        break;
-        }
-    }
-    // The className is not found
-    if (length === classes.length) {
-        classes.push(className);
-    }
+	for (; i < length; i++) {
+		if (classes[i] === className) {
+			classes.splice(i, 1);
+			break;
+		}
+	}
+	// The className is not found
+	if (length === classes.length) {
+		classes.push(className);
+	}
 
-    element.className = classes.join(' ');
+	element.className = classes.join(' ');
 }
 
 function toggleAll(e: MouseEvent) {
-    var active = 'active';
+	const active = 'active';
 
-    e.preventDefault();
-    toggleClass(layout, active);
-    toggleClass(menu, active);
-    toggleClass(menuLink, active);
+	e.preventDefault();
+	toggleClass(layout, active);
+	toggleClass(menu, active);
+	toggleClass(menuLink, active);
 }
 
 if (menuLink === null) {
-    throw new Error("Variable menuLink can not be null!");
+	throw new Error("Variable menuLink can not be null!");
 }
 else {
-    menuLink.onclick = function (e) {
-        toggleAll(e);
-    };
+	menuLink.onclick = function (e) {
+		toggleAll(e);
+	};
 }
 
 if (content === null) {
-    throw new Error("Variable content can not be null!");
+	throw new Error("Variable content can not be null!");
 }
 else {
-    content.onclick = function(e) {
+	content.onclick = function (e) {
 
-        if (menu === null) {
-            throw new Error("Variable menu can not be empty!")
-        }
-        else {
-            if (menu.className.indexOf('active') !== -1) {
-                toggleAll(e);
-            }
-        }
-    };
+		if (menu === null) {
+			throw new Error("Variable menu can not be empty!")
+		}
+		else {
+			if (menu.className.indexOf('active') !== -1) {
+				toggleAll(e);
+			}
+		}
+	};
 }
